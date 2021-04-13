f85c6ef51be6e5516497a5a85c06650f57d67da8
Raju000000 <75058759+Raju000000@users.noreply.github.com>
2021-04-13 17:55:09 +0530

    Update Xplayer.py

--- Evie/modules/Xplayer.py
+++ Evie/modules/Xplayer.py
@@ -1,12 +1,1 @@
-from telethon import Button, events
 
-
-@register(pattern="^/ano")
-async def k(event):
- buttons= Button.inline('test', data='tp')
- text = 'anonindian'
- await event.reply(text, buttons=buttons)
-
-@tbot.on(events.CallbackQuery(pattern=r"tp"))
-async def cbot(event):
- await tbot.send_message(event.chat_id, f'{event.sender_id}')
