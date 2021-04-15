41370cd988a96b3fd59d2f4d156f0af9c1c99da8
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-15 19:51:35 +0530

    Update Song.py

--- Evie/modules/Song.py
+++ Evie/modules/Song.py
@@ -191,16 +191,107 @@ async def deezr(v_url):
  except:
      pass
 
-@register(pattern="^/saavn ?(.*)")
+@register(pattern="^/ytmusic ?(.*)")
 async def yt(event):
- input_str = event.pattern_match.group(1)
- pablo = await event.reply(f"Getting {input_str} From Youtube Servers. Please Wait.")
- if not input_str:
-     await pablo.edit(
-         "Please Give Me A Valid Input."
-     )
-     return
- 
+    ult = event
+    url = event.pattern_match.group(1)
+    x = await event.reply("Searching...")
+    if not url:
+        return await x.edit("**Error**\nUsage - `.song <song name>`")
+    search = SearchVideos(url, offset=1, mode="json", max_results=1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get("search_result")
+    try:
+        url = q[0]["link"]
+    except BaseException:
+        return await x.edit("`No matching song found...`")
+    type = "audio"
+    await x.edit(f"`Preparing to download {url}...`")
+    if type == "audio":
+        opts = {
+            "format": "bestaudio",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "writethumbnail": True,
+            "prefer_ffmpeg": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "320",
+                }
+            ],
+            "outtmpl": "%(id)s.mp3",
+            "quiet": True,
+            "logtostderr": False,
+        }
+    try:
+        await x.edit("`Getting info...`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await x.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await x.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await x.edit(
+            "`Video is not available from your geographic location due to"
+            + " geographic restrictions imposed by a website.`"
+        )
+        return
+    except MaxDownloadsReached:
+        await x.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await x.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await x.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        return await x.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+    except ExtractorError:
+        return await x.edit("`There was an error during info extraction.`")
+    except Exception as e:
+        return await x.edit(f"{str(type(e)): {str(e)}}")
+    dir = os.listdir()
+    if f"{rip_data['id']}.mp3.jpg" in dir:
+        thumb = f"{rip_data['id']}.mp3.jpg"
+    elif f"{rip_data['id']}.mp3.webp" in dir:
+        thumb = f"{rip_data['id']}.mp3.webp"
+    else:
+        thumb = None
+    upteload = """
+Uploading...
+Song name - {}
+By - {}
+""".format(
+        rip_data["title"], rip_data["uploader"]
+    )
+    await x.edit(f"`{upteload}`")
+    await tbot.send_file(
+        ult.chat_id,
+        f"{rip_data['id']}.mp3",
+        thumb=thumb,
+        supports_streaming=True,
+        attributes=[
+            DocumentAttributeAudio(
+                duration=int(rip_data["duration"]),
+                title=str(rip_data["title"]),
+                performer=str(rip_data["uploader"]),
+            )
+        ],
+    )
+    await x.delete()
+    os.remove(f"{rip_data['id']}.mp3")
+    try:
+        os.remove(thumb)
+    except BaseException:
+        pass
     
 
 
